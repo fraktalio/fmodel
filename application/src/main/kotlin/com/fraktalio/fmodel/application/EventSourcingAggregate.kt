bcd77863fef3353875a12e8cabaada1a498836b1
Ivan Dugalic <ivan.dugalic@axoniq.io>
2021-07-09 19:34:36 +0200

    Added Saga to the Aggregate components
    API changed: StateStoredAggregate is returning `StateStoredSuccessfully` now.

--- application/src/main/kotlin/com/fraktalio/fmodel/application/EventSourcingAggregate.kt
+++ application/src/main/kotlin/com/fraktalio/fmodel/application/EventSourcingAggregate.kt
@@ -19,10 +19,14 @@ package com.fraktalio.fmodel.application
 import arrow.core.Either
 import arrow.core.computations.either
 import com.fraktalio.fmodel.domain.Decider
+import com.fraktalio.fmodel.domain.Saga
+import com.fraktalio.fmodel.domain._Saga
 
 /**
- * Event sourcing aggregate is using/delegating a [Decider] to handle commands and produce events.
- * In order to handle the command, aggregate needs to fetch the current state (represented as a list of events) via [EventRepository.fetchEvents] function, and then delegate the command to the decider which can produce new event(s) as a result.
+ * 
+ * Event sourcing aggregate is using/delegating a [EventSourcingAggregate.decider] of type [Decider]<[C], [S], [E]> to handle commands and produce events.
+ * In order to handle the command, aggregate needs to fetch the current state (represented as a list of events) via [EventRepository.fetchEvents] function, and then delegate the command to the [EventSourcingAggregate.decider] which can produce new event(s) as a result.
+ * If the [EventSourcingAggregate.decider] is combined out of many deciders via `combine` function, an optional [EventSourcingAggregate.saga] could be used to react on new events and send new commands to the [EventSourcingAggregate.decider] recursively, in one transaction.
  * Produced events are then stored via [EventRepository.save] suspending function.
  *
  * [EventSourcingAggregate] implements an interface [EventRepository] by delegating all of its public members to a specified object.
@@ -33,13 +37,15 @@ import com.fraktalio.fmodel.domain.Decider
  * @param E Events of type [E] that this aggregate can publish
  * @property decider A decider component of type [Decider]<[C], [S], [E]>.
  * @property eventRepository Interface for [E]vent management/persistence - dependencies by delegation
+ * @property saga A saga component of type [Saga]<[E], [C]>
  * @constructor Creates [EventSourcingAggregate]
  *
  * @author Иван Дугалић / Ivan Dugalic / @idugalic
  */
 data class EventSourcingAggregate<C, S, E>(
     private val decider: Decider<C, S, E>,
-    private val eventRepository: EventRepository<C, E>
+    private val eventRepository: EventRepository<C, E>,
+    private val saga: Saga<E, C>? = null
 ) : EventRepository<C, E> by eventRepository {
 
     /**
@@ -51,13 +57,31 @@ data class EventSourcingAggregate<C, S, E>(
     suspend fun handle(command: C): Either<Error, Iterable<Success.EventStoredSuccessfully<E>>> =
         // Arrow provides a Monad instance for Either. Except for the types signatures, our program remains unchanged when we compute over Either. All values on the left side assume to be Right biased and, whenever a Left value is found, the computation short-circuits, producing a result that is compatible with the function type signature.
         either {
-            val events = command.fetchEvents().bind()
-            val state = events.fold(decider.initialState, decider.evolve).validate().bind()
-            decider.decide(command, state).save().bind()
+            command
+                .fetchEvents().bind()
+                .calculateNewEvents(command).bind()
+                .save().bind()
         }
 
     private fun S.validate(): Either<Error, S> =
         if (decider.isTerminal(this)) Either.Left(Error.AggregateIsInTerminalState(this))
         else Either.Right(this)
+
+    private suspend fun Iterable<E>.calculateNewEvents(command: C): Either<Error, Iterable<E>> =
+        either {
+            val currentEvents = this@calculateNewEvents
+            val currentState = currentEvents.fold(decider.initialState, decider.evolve).validate().bind()
+            var newEvents = decider.decide(command, currentState)
+
+            if (saga != null) newEvents
+                .flatMap { saga.react(it) }
+                .forEach {
+                    newEvents = newEvents.plus(
+                        currentEvents.plus(newEvents)
+                            .calculateNewEvents(it).bind()
+                    )
+                }
+            newEvents
+        }
 
 }
--- application/src/main/kotlin/com/fraktalio/fmodel/application/StateStoredAggregate.kt
+++ application/src/main/kotlin/com/fraktalio/fmodel/application/StateStoredAggregate.kt
@@ -17,12 +17,15 @@
 package com.fraktalio.fmodel.application
 
 import arrow.core.Either
+import arrow.core.Either.*
 import arrow.core.computations.either
 import com.fraktalio.fmodel.domain.Decider
+import com.fraktalio.fmodel.domain.Saga
 
 /**
- * State stored aggregate is using/delegating a [Decider] to handle commands and produce new state.
- * In order to handle the command, aggregate needs to fetch the current state via [StateRepository.fetchState] function first, and then delegate the command to the decider which can produce new state as a result.
+ * State stored aggregate is using/delegating a [StateStoredAggregate.decider] of type [Decider]<[C], [S], [E]> to handle commands and produce new state.
+ * In order to handle the command, aggregate needs to fetch the current state via [StateRepository.fetchState] function first, and then delegate the command to the [StateStoredAggregate.decider] which can produce new state as a result.
+ * If the [StateStoredAggregate.decider] is combined out of many deciders via `combine` function, an optional [StateStoredAggregate.saga] could be used to react on new events and send new commands to the [StateStoredAggregate.decider] recursively, in one transaction.
  * New state is then stored via [StateRepository.save] suspending function.
  *
  * [StateStoredAggregate] implements an interface [StateRepository] by delegating all of its public members to a specified object.
@@ -33,13 +36,15 @@ import com.fraktalio.fmodel.domain.Decider
  * @param E Events of type [E] that are used internally to build/fold new state
  * @property decider A decider component of type [Decider]<[C], [S], [E]>.
  * @property stateRepository Interface for [S]tate management/persistence - dependencies by delegation
+ * @property saga A saga component of type [Saga]<[E], [C]>
  * @constructor Creates [StateStoredAggregate]
  *
  * @author Иван Дугалић / Ivan Dugalic / @idugalic
  */
 data class StateStoredAggregate<C, S, E>(
     private val decider: Decider<C, S, E>,
-    private val stateRepository: StateRepository<C, S>
+    private val stateRepository: StateRepository<C, S>,
+    private val saga: Saga<E, C>? = null
 ) : StateRepository<C, S> by stateRepository {
 
     /**
@@ -48,17 +53,25 @@ data class StateStoredAggregate<C, S, E>(
      * @param command Command message of type [C]
      * @return Either [Error] or [Success]
      */
-    suspend fun handle(command: C): Either<Error, Success.StateStoredAndEventsPublishedSuccessfully<S, E>> =
+    suspend fun handle(command: C): Either<Error, Success.StateStoredSuccessfully<S>> =
         // Arrow provides a Monad instance for Either. Except for the types signatures, our program remains unchanged when we compute over Either. All values on the left side assume to be Right biased and, whenever a Left value is found, the computation short-circuits, producing a result that is compatible with the function type signature.
         either {
-            val state = (command.fetchState().bind() ?: decider.initialState).validate().bind()
-            val events = decider.decide(command, state)
-            events.fold(state, decider.evolve).save()
-                .map { s -> Success.StateStoredAndEventsPublishedSuccessfully(s.state, events) }.bind()
+            (command.fetchState().bind() ?: decider.initialState)
+                .calculateNewState(command).bind()
+                .save().bind()
         }
 
-    private fun S.validate(): Either<Error, S> =
-        if (decider.isTerminal(this)) Either.Left(Error.AggregateIsInTerminalState(this))
-        else Either.Right(this)
+    private fun S.validateIfTerminal(): Either<Error, S> =
+        if (decider.isTerminal(this@validateIfTerminal)) Left(Error.AggregateIsInTerminalState(this@validateIfTerminal))
+        else Right(this@validateIfTerminal)
+
+    private suspend fun S.calculateNewState(command: C): Either<Error, S> =
+        either {
+            val currentState = this@calculateNewState.validateIfTerminal().bind()
+            val events = decider.decide(command, currentState)
+            val newState = events.fold(this@calculateNewState, decider.evolve)
+            if (saga != null) events.flatMap { saga.react(it) }.forEach { newState.calculateNewState(it) }
+            newState
+        }
 }
 
