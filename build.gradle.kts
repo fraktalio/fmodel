0b22d530ccc6c4fc8fa0d08a77d4ca1fb430cfdf
Ivan Dugalic <idugalic@gmail.com>
2022-02-22 22:56:03 +0100

    Using Node 16.* support Apple ARM.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 
 }
 
-
 allprojects {
     group = "com.fraktalio.fmodel"
     version = "3.0.1-SNAPSHOT"
@@ -19,3 +18,7 @@ allprojects {
     }
 }
 
+// see https://youtrack.jetbrains.com/issue/KT-49109#focus=Comments-27-5381158.0-0
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.13.1"
+}
