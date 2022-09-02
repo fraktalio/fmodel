0f11b914e2592a69bd53f6a3acf8900d402e5043
Ivan Dugalic <idugalic@gmail.com>
2022-09-02 11:59:34 +0200

    All native targets published by Github Actions

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -17,9 +17,14 @@ kotlin {
         nodejs()
     }
     val hostOs = System.getProperty("os.name")
+    val arch = System.getProperty("os.arch")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64()
+        hostOs == "Mac OS X" ->
+            when {
+                arch == "aarch64" || arch.startsWith("arm") -> macosArm64()
+                else -> macosX64()
+            }
         hostOs == "Linux" -> linuxX64()
         isMingwX64 -> mingwX64()
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
