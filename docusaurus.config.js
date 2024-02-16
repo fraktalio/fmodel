ad85236e36cf1313454683838462633c67072b86
Ivan Dugalic <idugalic@gmail.com>
2024-02-16 22:48:42 +0100

    title changed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Fmodel',
+    title: 'f { model }',
     tagline: 'Accelerate development of compositional, safe and ergonomic applications',
     favicon: 'img/favicon-32x32.png',
 
